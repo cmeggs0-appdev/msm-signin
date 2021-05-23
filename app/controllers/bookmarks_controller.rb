9c9b76b63ce28b928a6852acf8f543f9224c2381
cmeggs0 <cmeggs0@chicagobooth.edu>
2021-05-23 03:43:14 +0000

    dropdown form works

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,12 @@
 class BookmarksController < ApplicationController
   def index
-    matching_bookmarks = Bookmark.all
+    matching_bookmarks = Bookmark.where(:user_id == session[:user_id])
 
     @list_of_bookmarks = matching_bookmarks.order({ :created_at => :desc })
+
+    matching_movies = Movie.all
+
+    @list_of_movies = matching_movies.order({ :created_at => :desc })
 
     render({ :template => "bookmarks/index.html.erb" })
   end
@@ -19,7 +23,7 @@ class BookmarksController < ApplicationController
 
   def create
     the_bookmark = Bookmark.new
-    the_bookmark.user_id = params.fetch("query_user_id")
+    the_bookmark.user_id = session[:user_id]
     the_bookmark.movie_id = params.fetch("query_movie_id")
 
     if the_bookmark.valid?
